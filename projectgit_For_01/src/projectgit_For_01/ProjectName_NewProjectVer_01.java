cc0781f3637ca941752033ca279ab22ad0510eee
k2k1997 <j59160859@gmail.com>
2022-05-15 22:18:59 +0900

    This is my first TeamProject!

---
+++ projectgit_For_01/src/projectgit_For_01/ProjectName_NewProjectVer_01.java
@@ -0,0 +1,14 @@
+package projectgit_For_01;
+class A{
+	int a;
+	
+	public void abc(){
+		System.out.println("Hi");
+	}
+}
+public class ProjectName_NewProjectVer_01 {
+public static void main(String[] args) {
+	A a = new A();
+	a.abc();
+}
+}
